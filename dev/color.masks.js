3d5178bc66ad31c8bf892559020297313d472ad9
Roman Melnyk <email.rom.melnyk@gmail.com>
2015-01-27 15:11:56 +0200

    added the 'i' option for RegExps

--- dev/color.masks.js
+++ dev/color.masks.js
@@ -3,9 +3,9 @@
  */
 ;(function (color) {
 	color.masks = {};
-	color.masks.hsl 	= /^ *hsl\( *(\d{1,3}), *(100|\d{1,2}%), *(100|\d{1,2}%) *\) *$/;
-	color.masks.hsla 	= /^ *hsla\( *(\d{1,3}), *(100|\d{1,2}%), *(100|\d{1,2}%), *(1|0|\.\d+|0\.\d+) *\) *$/;
-	color.masks.rgb 	= /^ *rgb\( *(\d{1,3}), *(\d{1,3}), *(\d{1,3}) *\) *$/;
-	color.masks.rgba 	= /^ *rgba\( *(\d{1,3}), *(\d{1,3}), *(\d{1,3}), *(1|0|\.\d+|0\.\d+) *\) *$/;
-	color.masks.rgbhex 	= /^ *#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6}) *$/;
+	color.masks.hsl 	= /^ *hsl\( *(\d{1,3}), *(100|\d{1,2}%), *(100|\d{1,2}%) *\) *$/i;
+	color.masks.hsla 	= /^ *hsla\( *(\d{1,3}), *(100|\d{1,2}%), *(100|\d{1,2}%), *(1|0|\.\d+|0\.\d+) *\) *$/i;
+	color.masks.rgb 	= /^ *rgb\( *(\d{1,3}), *(\d{1,3}), *(\d{1,3}) *\) *$/i;
+	color.masks.rgba 	= /^ *rgba\( *(\d{1,3}), *(\d{1,3}), *(\d{1,3}), *(1|0|\.\d+|0\.\d+) *\) *$/i;
+	color.masks.rgbhex 	= /^ *#?([0-9a-f]{3}|[0-9a-f]{6}) *$/i;
 })(window.$color);
